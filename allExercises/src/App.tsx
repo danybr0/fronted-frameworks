9ef2b239c8fd7dcad20fcad63a924a0f57fef7a1
danybr0 <dan.covic@outlook.com>
2023-10-22 23:13:29 +0200

    Les 1 - Oefening: Voorbereiding: Nieuw project & basisstructuur

---
+++ allExercises/src/App.tsx
@@ -0,0 +1,16 @@
+import Exercise from "./les1_components/exercise.tsx"
+
+function App() {
+
+  return (
+    <>
+      <Exercise title={'Exercise 1: Multiplication Table'}/>
+      <Exercise title={'Exercise 2: Rater'} background={'#77EEEE'}/>
+      <Exercise title={'Exercise 3: Progress Bar'}/>
+      <Exercise title={'Exercise 4: Number Grid'} background={'#77EEEE'}/>
+      <Exercise title={'Exercise 5: Comment Card'}/>
+    </>
+  )
+}
+
+export default App
